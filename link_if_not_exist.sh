7ee608f72f842a0088e7e95465bb304b3d7fb112
QisNwt <qisnwt@gmail.com>
2012-04-30 20:36:51 +0000

    Modified link scripts.

--- link_if_not_exist.sh
+++ link_if_not_exist.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-for item in `ls -a1 | grep "^\." | egrep -v "^\.+$"`; do
-    if [ ! -e "${HOME}/${item}" ]; then
-         ln -s ${PWD}/${item} "${HOME}/${item}";
-         echo "linked to ${PWD}/${item} from ${HOME}/${item}";
-    else
-         echo "ignored:  ${PWD}/${item} (${HOME}/${item} already exists)";
-    fi
+for item in .?*; do
+    case $item in
+        .. | .git)
+            continue;;
+        *)
+            if [ ! -e "${HOME}/${item}" ]; then
+                ln -s ${PWD}/${item} "${HOME}/${item}";
+                echo "linked to ${PWD}/${item} from ${HOME}/${item}";
+            else
+                echo "ignored:  ${PWD}/${item} (${HOME}/${item} already exists)";
+            fi
+            ;;
+    esac
 done
 echo 'done.';
