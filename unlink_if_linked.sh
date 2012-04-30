cf193cdb122ebf153b31a290358104ea9aa290d2
QisNwt <qisnwt@gmail.com>
2012-04-30 21:05:39 +0000

    Renamed 'unlink_if_linked.sh'.

---
+++ unlink_if_linked.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+for item in .*?; do
+    case ${item} in
+        .. | .git )
+            continue;;
+        *)
+            if [ 0 -eq `file "${HOME}/${item}" | grep "${PWD}/${item}" | echo $?` ]; then
+                unlink "${HOME}/${item}";
+                echo "deleted link to ${PWD}/${item} from ${HOME}/${item}";
+            fi
+            ;;
+    esac
+done
+echo 'done.';
